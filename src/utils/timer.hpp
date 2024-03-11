141a5b8bd8e68ee047fc574f3455e4a6e15ffab2
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2024-03-11 16:30:58 +0000

    Use std::chrono for time measurements

--- src/utils/timer.hpp
+++ src/utils/timer.hpp
@@ -1,14 +1,13 @@
 #ifndef STELLAR_TIME_H
 #define STELLAR_TIME_H
 
-#include <sys/time.h>
+#include <chrono>
 
 namespace timer {
 
 inline uint32_t get_ms() {
-    struct timeval time{};
-    gettimeofday(&time, nullptr);
-    return time.tv_sec * 1000 + time.tv_usec / 1000;
+    const auto duration = std::chrono::system_clock::now().time_since_epoch();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
 }
 
 } // namespace timer
